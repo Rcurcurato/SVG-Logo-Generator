224ebb17d9ec367eb0d5f62b7f6e182f44527bfc
Rcurcurato <133296185+Rcurcurato@users.noreply.github.com>
2023-08-18 19:19:13 -0400

    added file.js file

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+//index.js: This file will be your entry point where you collect user input,
+// generate SVG shapes, and write to files.
+
+//lib/shapes.js: This module could contain the logic for generating SVG shapes based on the user's choices.
+
+//lib/file.js: This module could contain the logic for writing SVG data to a file.
+
 const inquirer = require('inquirer');
-const fs = require('fs')
-const Shape = require('./shapes');
+const Shapes = require('./lib/shapes');
+const writeToFile = require('./lib.file');
 
 const questions = [
     {
         type: 'input',
-        name: 'font color',
+        name: 'title',
+        message: 'please enter text for your logo (enter up to three characters)',
+        validate: value => {
+            if (value) {
+                return true;
+            } else {
+                console.log('please choose a font color')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'fontColor',
         message: 'What color would you like your font to be?',
         validate: value => {
             if (value) {
@@ -18,7 +38,7 @@ const questions = [
 },
     {
         type: 'list',
-        name: 'shape choice',
+        name: 'shapeChoice',
         message: 'What shape would you like to use?',
         choices: [
             'circle',
@@ -29,18 +49,21 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'shape color',
-        message: 'what color would you like your shape to be?'
+        name: 'shapeColor',
+        message: 'what color would you like your shape to be?',
+        validate: value => {
+            if (value) {
+                return true;
+            } else {
+                console.log('please choose a font color')
+                return false;
+            }
+        }
     },
 
-    function writeToFile(fileName, data) {
-        fs.writeFile(fileName, data, err => {
-            if (err) throw err;
-    
-            console.log("SVG file created!")
-        });
-    },
 ];
 
     // Function call to initialize app
 init();
+
+
